9c6a7c6495a16181a60253999ad94fdb3ef815d1
HyperJeanJean <75644348+HyperJeanJean@users.noreply.github.com>
2025-06-24 13:12:13 +0200

    Set a saner default value for snow line height

--- src/tile_type.h
+++ src/tile_type.h
@@ -30,7 +30,7 @@ static const uint MIN_MAP_HEIGHT_LIMIT = 15;                   ///< Lower bound 
 static const uint MAX_MAP_HEIGHT_LIMIT = MAX_TILE_HEIGHT;      ///< Upper bound of maximum allowed heightlevel (in the construction settings)
 
 static const uint MIN_SNOWLINE_HEIGHT = 2;                     ///< Minimum snowline height
-static const uint DEF_SNOWLINE_HEIGHT = 10;                    ///< Default snowline height
+static const uint DEF_SNOWLINE_HEIGHT = 4;                     ///< Default snowline height
 static const uint MAX_SNOWLINE_HEIGHT = (MAX_TILE_HEIGHT - 2); ///< Maximum allowed snowline height
 
 static const uint DEF_DESERT_COVERAGE = 50;                    ///< Default desert coverage.
